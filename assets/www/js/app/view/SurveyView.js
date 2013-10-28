8d322cecf67996c629444db74d36d3962adde9cf
Flavio Alves <flaviof.alves@gmail.com>
2013-10-28 18:30:05 -0200

    TAG: CARLTON HOTEL prototype

--- assets/www/js/app/view/SurveyView.js
+++ assets/www/js/app/view/SurveyView.js
@@ -3,17 +3,16 @@
   window.SurveyView = Backbone.View.extend({
 
     events : {
-      'click .frequency' : 'handleFrequencyQuestion',
-      'click .returning' : 'handleReturningClientQuestion',
-      'click .good-reason' : 'handleGoodReasonQuestion',
-      'click .bad-reason' : 'handleBadReasonQuestion',
       'click .nps' : 'handleNPSQuestion',
-      'click .demographic' : 'handleDemographicQuestion',
-      'click .leave-email' : 'handleEmailQuestion',
-      'click .leave-sugestion' : 'handleLeaveSugestionQuestion',
-      'click .submit-sugestion, .cancel-sugestion' : 'handleSubmitSurvey',
-      'keydown :input.email-input' : 'autoCompleteEmail',
-      'click #email_sugestion_link, #claim_email_sugestion_link' : 'selectEmailSuggestion',
+      'click .checkin' : 'handleCheckinQuestion',
+      'click .room' : 'handleRoomQuestion',
+      'click .public-areas' : 'handlePublicAreasQuestion',
+      'click .bar' : 'handleBarQuestion',
+      'click .staff' : 'handleStaffQuestion',
+      'click .returning' : 'handleReturningQuestion',
+      'click .email' : 'handleEmailQuestion',
+      'click .sugestion' : 'handleSugestionQuestion',
+      'keydown :input.email-input' : 'autoCompleteEmail',      
       'focus :input.email-input, :input.feedback-input' : 'hideHeaderAndFooter',
       'focusout :input.email-input, :input.feedback-input' : 'showHeaderAndFooter',
     },
@@ -21,114 +20,85 @@
     render : function() {
       console.log(" == BACKBONE VIEW ROCKS == ");
       $('#email_sugestion').hide();
-      $('#claim_email_sugestion').hide();
       return this;
     },
 
-    handleFrequencyQuestion : function(e){
-      $(this.el).off('click', '.frequency');
-      //TODO: SET DATA IN MODEL
-      App.utils.setMarkedButton(e);
-      App.config.DATA_SURVEY = { frequency: e.currentTarget.innerText, confirmed_sended: 0, origin: "totem" };
-      
-      if(App.config.DATA_SURVEY.frequency === "Primeira vez") {
-        App.utils.nextQuestion($("#returning-question"));        
-      } else{
-        App.config.DATA_SURVEY.want_return = null;  
-        App.utils.nextQuestion($("#good-reason-question"));
-      }    
-    },
-
-    handleReturningClientQuestion : function(e){
-      $(this.el).off('click', '.returning');
-      App.config.DATA_SURVEY.want_return = e.currentTarget.innerText;      
-      App.utils.setMarkedButton(e);
-      App.utils.nextQuestion($("#good-reason-question"));
-    },
-
-    handleGoodReasonQuestion : function(e){
-      $(this.el).off('click', '.good-reason');
-      //TODO: SET DATA IN MODEL
-      App.config.DATA_SURVEY.good_reason = e.currentTarget.innerText;            
-      App.utils.setMarkedButton(e);
-      App.utils.nextQuestion("#bad-reason-question")
-    },
-
-    handleBadReasonQuestion : function(e){    
-      $(this.el).off('click', '.bad-reason');
-      App.config.DATA_SURVEY.bad_reason = e.currentTarget.innerText;
-      App.utils.setMarkedButton(e);
-      App.utils.nextQuestion($("#nps-question"));
-    },
 
     handleNPSQuestion : function(e){
-      $(this.el).off('click', '.nps');
-      App.config.DATA_SURVEY.nps = e.currentTarget.innerText;
+      $(this.el).off('click', '.nps');    
+      App.config.DATA_SURVEY = { nps: e.currentTarget.innerText, confirmed_sended: 0, origin: "qrcode" };
 
       App.utils.setMarkedButton(e);
-      App.utils.nextQuestion($("#demographic-question"));
-    },  
+      App.utils.nextQuestion($("#checkin-question"));
+    },
 
-    handleDemographicQuestion : function(e){
-      if($(e.currentTarget).hasClass("age")){                        
-        $('.age').removeClass("selected")
-        App.config.DATA_SURVEY.age = e.currentTarget.innerText;
+    handleCheckinQuestion : function(e){
+      $(this.el).off('click', '.checkin');    
+      App.config.DATA_SURVEY.room =  e.currentTarget.innerText;
 
-      } else {
-        $('.sex').removeClass("selected")
-        App.config.DATA_SURVEY.sex = e.currentTarget.innerText;
-      }
-    
       App.utils.setMarkedButton(e);
+      App.utils.nextQuestion($("#room-question"));
+    },
 
-      //just go to next question if both params are completed
-      if(App.config.DATA_SURVEY.age != null && App.config.DATA_SURVEY.sex != null){
-        if(App.config.DATA_SURVEY.nps > 6) {
-          App.utils.nextQuestion($("#email-question"));
-        } else{
-          App.utils.nextQuestion($("#claim-email-question"));
-        }
-      }
+    handleRoomQuestion : function(e){
+      $(this.el).off('click', '.room');    
+      App.config.DATA_SURVEY.room =  e.currentTarget.innerText;
+
+      App.utils.setMarkedButton(e);
+      App.utils.nextQuestion($("#public-areas-question"));
+    },
+
+    handlePublicAreasQuestion : function(e){
+      $(this.el).off('click', '.public-areas');    
+      App.config.DATA_SURVEY.publicAreas =  e.currentTarget.innerText;
+
+      App.utils.setMarkedButton(e);
+      App.utils.nextQuestion($("#bar-question"));
+    },
+
+    handleBarQuestion : function(e){
+      $(this.el).off('click', '.bar');    
+      App.config.DATA_SURVEY.bar =  e.currentTarget.innerText;
+
+      App.utils.setMarkedButton(e);
+      App.utils.nextQuestion($("#staff-question"));
+    },
+
+    handleStaffQuestion : function(e){
+      $(this.el).off('click', '.staff');    
+      App.config.DATA_SURVEY.staff =  e.currentTarget.innerText;
+
+      App.utils.setMarkedButton(e);
+      App.utils.nextQuestion($("#return-question"));
+    },
+
+    handleReturningQuestion : function(e){
+      $(this.el).off('click', '.returning');    
+      App.config.DATA_SURVEY.returning =  e.currentTarget.innerText;
+
+      App.utils.setMarkedButton(e);
+      App.utils.nextQuestion($("#email-question"));
     },
 
     handleEmailQuestion : function(e){
-      $(this.el).off('click', '.leave-email');
-      var el = $(this).parent();
-      var email = null;
-      if($(this.el).find("input[name='user_contact']").val()){
-        email = $(this.el).find("input[name='user_contact']").val();
-      } else {
-        email = $(this.el).find("input[name='claim_user_contact']").val();
-      }
-      App.config.DATA_SURVEY.email = email;
+      $(this.el).off('click', '.email');    
+      App.config.DATA_SURVEY.email =  e.currentTarget.innerText;
+
       App.utils.setMarkedButton(e);
       App.utils.nextQuestion($("#leave-sugestion"));
     },
 
-    handleLeaveSugestionQuestion : function(e){
-      $(this.el).off('click', '.email-question');      
-      App.utils.setMarkedButton(e);
-
-      if($(e.currentTarget).hasClass('yes')){
-        App.utils.nextQuestion($("#sugestion-box"));
-      } else{
-        App.config.DATA_SURVEY.sugestion = null;
-        App.utils.finishSurvey();
-      }
-    },
-
-    handleSubmitSurvey : function(e){
-      $(this.el).off('click', '.submit-sugestion, .cancel-sugestion');   
-
-      App.utils.setMarkedButton(e);    
+    handleSugestionQuestion : function(e){
+      $(this.el).off('click', '.sugestion');    
+      
       var feedback = $(this.el).find("textarea[name='feedback']").val();
       App.config.DATA_SURVEY.sugestion = feedback;
-
-      console.log("---------")
-      console.log(" FEEDBACK = " + feedback)
-
       App.utils.finishSurvey();
-    },
+
+      App.utils.setMarkedButton(e);
+      App.utils.finishSurvey();
+      //App.utils.nextQuestion($("#thanks-message"));
+    },    
 
     autoCompleteEmail : function(e){
       $('#email_sugestion').hide();
@@ -144,15 +114,7 @@
         },
         empty: function(element) {
         }
-    });
-    },    
-
-    selectEmailSuggestion : function(e){
-      e.preventDefault();
-      var currentEl = $(e.currentTarget);
-      var emailSuggested = currentEl.html();
-      currentEl.parent().parent().children('input[name=user_contact]').val(emailSuggested);
-      currentEl.parent().hide();
+      });
     },
 
     hideHeaderAndFooter : function(){
@@ -164,8 +126,8 @@
     showHeaderAndFooter : function(){
       $(".l-footer-survey").show();
       $('.l-header-survey').show();
-    }    
+    }   
 
   });
-    
+
 })();
